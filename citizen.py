b6c819fc06181fe2d678d16440de035447f44c73
Fadi Yousef <fadi721994@gmail.com>
2019-04-11 19:59:55 +0200

    Step 1

--- citizen.py
+++ citizen.py
@@ -1,6 +1,15 @@
 class Citizen:
-    def __init__(self, str='', fitness=0, age=0):
+    def __init__(self, str='', fitness=0, age=0, board=None, knapsack=None, cap=0):
         self.str = str
+        if board is None:
+            self.board = []
+        else:
+            self.board = board
+        if knapsack is None:
+            self.knapsack = []
+        else:
+            self.knapsack = knapsack
+        self.capacity = cap
         self.fitness = fitness
         self.age = age
 
---
+++ genetic_algorithm.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class GeneticAlgorithm(Enum):
+    StringSearch = 0
+    NQueens = 1
+    Knapsack = 2
